aaba5c342e009d7e1f7046889a42def903ebd5b9
Raoni Demarchi <raonidemarchi@gmail.com>
2019-06-17 18:30:06 -0300

    Finish document view page

--- src/components/DocumentDetails/DocumentDetails.js
+++ src/components/DocumentDetails/DocumentDetails.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { pdfjs, Document, Page } from 'react-pdf';
 import SignatureCanvas from 'react-signature-canvas';
 
@@ -6,6 +6,8 @@ import styles from './DocumentDetails.module.scss';
 import './pdf-viewer.scss';
 
 import signatureIcon from '../../icons/signature-solid.svg';
+import paperPlane from '../../icons/paper-plane-solid.svg';
+import eraserIcon from '../../icons/eraser-solid.svg';
 import pdf from '../../pdf/example.pdf';
 
 
@@ -13,20 +15,58 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 function DocumentDetails() {
   const [pagesCount, setPagesCount] = useState(0);
-  const [modalOpen, setModalOpen] = useState(false);
+  const [signed, setSigned] = useState(false);
   let signaturePad = {};
+  let signaturePage = {};
 
-  useEffect(() => {
-    signaturePad.clear()
-  })
+  window.onresize = function() {
+    if (signed) {
+      setSigned(false);
+    }
+  }
 
   const onDocumentLoadSuccess = ({ numPages }) => {
     setPagesCount(numPages);
   }
 
-  const openSignatureModal = () => {
-    setModalOpen(true);
-    signaturePad.clear()
+  const goToSignatureArea = () => {
+    window.scrollTo(0, document.body.scrollHeight);
+    
+    signaturePage.classList.remove(styles.highlight);
+
+    // highlights signature area
+    setTimeout(() => {
+      signaturePage.className += ' ' + styles.highlight;
+    }, 200)
+  }
+
+  const changeButtonToSend = () => {
+    setSigned(!signaturePad.isEmpty());
+  }
+
+  const clearSignaturePad = () => {
+    signaturePad.clear();
+    setSigned(false);
+  }
+
+  const sendSignedDocument = () => {
+    fetch('https://pdfgen.catini.org', {
+        method: 'post',
+        body: JSON.stringify({
+          html: `<div style="width: 100%">
+          <div style="margin: 0 auto">
+            <img src="${signaturePad.getTrimmedCanvas().toDataURL('image/png')}" width="400" />
+          <div>
+          </div>
+          `,
+          filename: 'Raoni'
+        })
+      })
+      .then(res => res.json())
+      .then(
+        (result) => { window.open(result.url) },
+        (error) => {  }
+      )
   }
 
   return (
@@ -43,26 +83,42 @@ function DocumentDetails() {
             )}
           </Document>
 
-          <div className={styles.signaturePage}>
+          <div className={styles.signaturePage} ref={ref => { signaturePage = ref }}>
             <SignatureCanvas
               penColor="black"
               canvasProps={{ className: styles.signatureArea }}
               ref={ref => { signaturePad = ref }}
+              onEnd={changeButtonToSend}
             />
 
             <div className={styles.signerDetails}>
-              <p>Nome do cliente</p>
-              <p>Empresa do cliente</p>
+              <hr className={styles.signatureRow} />
+              <p>
+                <b>Laércio Cosentino</b>
+                <br />
+                TOTVS
+              </p>
             </div>
           </div>
         </div>
       </div>
 
-      <div className={styles.fab} onClick={openSignatureModal}>
-        <img className={styles.signatureIcon} src={signatureIcon} alt="Signature icon" /> Assinar proposta
-      </div>
+      { 
+        signed ?
+          <div>
+            <div className={styles.fabClear} title="Apagar assinatura" onClick={clearSignaturePad}>
+              <img className={styles.buttonIcon} src={eraserIcon} alt="Eraser icon" />
+            </div>
 
-      
+            <div className={styles.fabSend} title="Enviar proposta assinada" onClick={sendSignedDocument}>
+              <img className={styles.buttonIcon} src={paperPlane} alt="Paper plane icon" /> Enviar
+            </div>
+          </div>
+        :
+          <div className={styles.fabGoSignature} onClick={goToSignatureArea}>
+            <img className={styles.buttonIcon} src={signatureIcon} alt="Signature icon" /> Ir para assinatura
+          </div>
+      }
     </div>
   );
 }

1bfb0619a64c9a9e8f7e89d96e0b138b567d54c8
Raoni Demarchi <raonidemarchi@gmail.com>
2019-06-17 21:01:59 -0300

    Remove TextEditor component

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,12 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
-const TextEditor = React.lazy(() => import('./components/TextEditor/TextEditor'))
 const DocumentDetails = React.lazy(() => import('./components/DocumentDetails/DocumentDetails'))
 
 const Routes = () => (
   <Router>
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
-        <Route exact path="/editor" component={props => <TextEditor {...props} />} />
         <Route exact path="/document" component={props => <DocumentDetails {...props} />} />
         <Redirect from="*" to="/document" />
       </Switch>
@@ -16,4 +14,4 @@ const Routes = () => (
   </Router>
 )
 
-export default Routes
+export default Routes;
